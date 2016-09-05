6a50c1acd5583300a8da0ebc34118a48cc0bdb01
Tariq Aziz <Tariq.Aziz0547@gmail.com>
2016-09-04 21:26:04 -0400

    Initial Commit

---
+++ Classroom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Classroom
+//
+//  Created by Tariq Aziz on 2016-09-04.
+//  Copyright © 2016 Tariq Aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
